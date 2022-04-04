c89772262110ae95deef61658832a0a7bf03a769
aersosi <aersosi@avodaq.com>
2022-04-04 11:42:50 +0200

    [improvement] - stuff

--- doku_libs/props/link_TWClasses.js
+++ doku_libs/props/link_TWClasses.js
@@ -1,3 +1,3 @@
 export const link_TWClasses = {
-    className: "bg-avo-pink-pink hover:bg-white text-white hover:text-avo-pink-pink transition px-8 py-4 font-bold text-xl",
+    className: "bg-avo-pink-pink hover:bg-avo-pink-400 text-white hover:text-white hover:-translate-y-1 hover:scale-105 hover:drop-shadow-xl transition px-8 py-4 font-bold text-xl",
 }
