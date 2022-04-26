704dba3dd2b1dda9152b4fe55172989e790b28de
aersosi <aersosi@avodaq.com>
2022-04-26 11:35:07 +0200

    [improvement] - stuff

--- doku_libs/props/fadeAnimation.js
+++ doku_libs/props/fadeAnimation.js
@@ -1,7 +1,33 @@
+import {keyframes} from "@emotion/react";
+
 export const fadeProps = {
-    direction: "up",
-    duration: 500,
+    duration: 750,
     damping: 0.25,
     cascade: true,
+    fraction: 0,
     triggerOnce: true
 }
+
+export const fadeUpProps = {
+    direction: "up",
+    duration: 750,
+    damping: 0.25,
+    cascade: true,
+    fraction: 0,
+    triggerOnce: true
+}
+
+export const customKeyframes = keyframes`
+  0% {
+    opacity: 0;
+    transform: translate3d(0, 50px, 0);
+  }
+
+  75% {
+    opacity: 1;
+  }
+
+  100% {
+    transform: translate3d(0, 0, 0);
+  }
+`;
